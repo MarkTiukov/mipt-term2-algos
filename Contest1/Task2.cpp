a5e5c1a92bfa290c53c63a14f5c3825c3d8a2630
MarkTiukov <tyukov.mark@gmail.com>
2020-02-24 01:06:47 +0300

    Passed YC

---
+++ Contest1/Task2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+bool checkForCycles(std::vector<std::vector<int>> &graph, int n);
+bool checkForCycles(std::vector<std::vector<int>> &graph, int n, int vertex, std::vector<int> &colors);
+
+std::vector<int>  answer;
+
+int main() {
+	int n, m;
+	std::cin >> n >> m;
+	std::vector<std::vector<int>> graph(n);
+	for (int i = 0; i < m; ++i) {
+		int from, to;
+		std::cin >> from >> to;
+		graph[from].push_back(to);
+	}
+	if (!checkForCycles(graph, n)) {
+		std::cout << "YES" << std::endl;
+		for (int i = 0; i < n; ++i) {
+			std::cout << answer[n - i - 1] << " ";
+		}
+		std::cout << std::endl;
+	} else {
+		std::cout << "NO" << std::endl;
+	}
+}
+
+bool checkForCycles(std::vector<std::vector<int>> &graph, int n) {
+	std::vector<int> colors(n); // 0 for "white"; 1 for "gray"; 2 for "black"
+	for (int i = 0; i < n; ++i) {
+		colors[i] = 0;
+	}
+	bool result = false;
+	for (int i = 0; !result && i < n; ++i) {
+		if (colors[i] ==  0) {
+			result = checkForCycles(graph, n, i, colors);
+		}
+	}
+	return result;
+}
+
+bool checkForCycles(std::vector<std::vector<int>> &graph, int n, int vertex, std::vector<int> &colors) {
+	colors[vertex] = 1;
+	bool result = false;
+	for (int i = 0; !result && i < graph[vertex].size(); ++i) {
+		int currentV = graph[vertex][i];
+		if (colors[currentV] == 1) {
+			result = true;
+			break;
+		}
+		if (colors[currentV] == 0)
+			result = checkForCycles(graph, n, currentV, colors);
+	}
+	colors[vertex] = 2;
+	answer.push_back(vertex);
+	return result;
+}
