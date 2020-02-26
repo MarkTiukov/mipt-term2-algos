1f9445ffbd3b0b6a13b44a70e9aa787d7eea5a33
MarkTiukov <tyukov.mark@gmail.com>
2020-02-26 15:16:21 +0300

    On the way to solve it

--- Contest1/Task3.cpp
+++ Contest1/Task3.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include <vector>
 
+struct Graph {
+	std::vector<std::vector<int>> graph;
+	int numberOfVertices = 0;
+
+	Graph(int n) {
+		this->numberOfVertices = n;
+		graph = std::vector<std::vector<int>>(n);
+	}
+
+	void add(int from, int to) {
+		this->graph[from].push_back(to);
+	}
+
+};
+
+void findSCC(std::vector<std::vector<int>> &graph, int n); // find strongly connected components
+
 int main() {
 	int n, m;
 	std::cin >> n >> m;
-	std::vector<std::vector<int>> graph(n);
+	Graph graph = Graph(n);
 	for (int i = 0; i < m; ++i) {
 		int from, to;
 		std::cin >> from >> to;
-		graph[from].push_back(to);
+		graph.add(from, to);
 	}
+	
+}
 
+void findSCC(std::vector<std::vector<int>> &graph, int n) {
+	
 }
