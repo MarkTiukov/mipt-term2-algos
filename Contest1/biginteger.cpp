dd0a53961ef31c48a5edcec8aed82d6ca7feb93c
MarkTiukov <tyukov.mark@gmail.com>
2020-03-04 13:42:13 +0300

    Wrote again. Constructors, totring() and operator <<

--- Contest1/biginteger.cpp
+++ Contest1/biginteger.cpp
@@ -4,6 +4,7 @@
 
 class BigInteger {
 	std::vector<int> number;
+	int sign = 0; // "0" --> 0, "1" --> positive, "-1" --> negative
 
 	int size() const {
 		return this->number.size();
@@ -21,6 +22,11 @@ class BigInteger {
 
 	BigInteger(int number) {
 		this->number = std::vector<int>();
+		if (number > 0)
+			this->sign = 1;
+		else
+			this->sign = 0 ? number == 0 : -1;
+		number = std::abs(number);
 		for (int i = 0; number / 10 > 0; ++i) {
 			this->number.push_back(number % 10);
 			number /= 10;
@@ -28,8 +34,14 @@ class BigInteger {
 		this->number.push_back(number);
 	}
 
+	BigInteger(const BigInteger &a) {
+		this->number = a.number;
+		this->sign = a.sign;
+	}
+
 	std::string toString() const {
 		std::string result = "";
+		result += this->sign  == -1 ? "-" : "";
 		for (int i = this->number.size() - 1; i >= 0; --i) {
 			result += std::to_string(this->number[i]);
 		}
@@ -37,12 +49,7 @@ class BigInteger {
 	}
 
 	BigInteger operator +=(const BigInteger& a) {
-		int remainder = 0;
-		for (int i = 0; i < number.size() || i < a.size(); ++i) {
-			this->number[i] += a.at(i) + remainder;
-			remainder = this->number[i] /= 10;
-			this->number[i] %= 10;
-		}
+
 	}
 
 };
@@ -56,7 +63,11 @@ std::ostream& operator <<(std::ostream& out, const BigInteger& a) {
 	return out;
 }
 
+std::istream& operator >>(std::istream& in, BigInteger& a) {
+
+}
+
 int main() {
-	BigInteger big = BigInteger(1234567890);
-	std::cout << big << std::endl;
+	BigInteger a = BigInteger(1241241);
+	BigInteger b = BigInteger(a);
 }
