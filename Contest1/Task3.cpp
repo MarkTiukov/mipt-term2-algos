bc9f65cc825b3709b907fdd50c01f4afa961b51f
MarkTiukov <tyukov.mark@gmail.com>
2020-03-11 19:31:07 +0300

    Fixed the searh for components

--- Contest1/Task3.cpp
+++ Contest1/Task3.cpp
@@ -4,7 +4,7 @@
 void findSCC(std::vector<std::vector<int>> &graph); // finds strongly connected components
 void dfs1(std::vector<std::vector<int>> &graph, int current, std::vector<int> &colors, std::vector<int> &vertices);
 void makeInvertedGraph(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &invertedGraph);
-bool dfs2(std::vector<std::vector<int>> &graph, int current, std::vector<int> &colors, std::vector<std::vector<int>> &components, const int &numberOfCurrentCC);
+void dfs2(std::vector<std::vector<int>> &graph, int current, std::vector<int> &colors, std::vector<std::vector<int>> &components, const int &numberOfCurrentCC);
 
 int main() {
 	int n, m;
@@ -20,7 +20,7 @@ int main() {
 
 void findSCC(std::vector<std::vector<int>> &graph) {
 	std::vector<int> outTime = std::vector<int>(graph.size());
-	std::vector<int> vertices; // stores vertices sorted by time of exiting'em
+	std::vector<int> vertices; // stores vertices sorted by time of exiting'em®
 	std::vector<int> colors(graph.size(), 0); // 0 = white; 1 = gray; 2 = black;
 	int time = 0;
 	for (int i = 0; i < graph.size(); ++i) {
@@ -30,19 +30,33 @@ void findSCC(std::vector<std::vector<int>> &graph) {
 	}
 	std::vector<std::vector<int>> invertedGraph(graph.size());
 	makeInvertedGraph(graph, invertedGraph);
+	/*
+	std::cout << "INVERTED GRAPH:" << std::endl;
+	for (int i = 0; i < invertedGraph.size(); ++i) {
+		for (int j : invertedGraph[i]) {
+			std::cout << i << " " << j << std::endl;
+		}
+	}
+	std::cout << "/////////////////////////" << std::endl;
+	 */
 	int ccWithOutEdges = 0;
 	int ccWithInEdges = 0;
 	colors = std::vector<int>(graph.size(), 0);
 	std::vector<std::vector<int>> components;
 	int numberOfCurrentCC = 0;
+	// checking vertices
+	/*
 	for (int i : vertices)
-		std::cout << i << std::endl;
-	for (int i = 0; i < vertices.size(); ++i) {
-		if (colors[i] == 0 && dfs2(invertedGraph, i, colors, components, numberOfCurrentCC)) {
-			ccWithInEdges++;
+	std::cout << i << std::endl;
+	*/
+	for (int i = vertices.size() - 1; i > -1; --i) {
+		if (colors[vertices[i]] == 0) {
+			dfs2(invertedGraph, vertices[i], colors, components, numberOfCurrentCC);
+			numberOfCurrentCC++;
 		}
-		numberOfCurrentCC++;
 	}
+	// printing components
+	/*
 	for (int i = 0; i < components.size(); ++i) {
 		std::cout << "Component number " << i << ": ";
 		for (int j = 0; j < components[i].size(); ++j) {
@@ -50,23 +64,19 @@ void findSCC(std::vector<std::vector<int>> &graph) {
 		}
 		std::cout << std::endl;
 	}
+	*/
 }
 
-bool dfs2(std::vector<std::vector<int>> &graph,
+void dfs2(std::vector<std::vector<int>> &graph,
 				 int current,
 				 std::vector<int> &colors,
 				 std::vector<std::vector<int>> &components,
 				 const int &numberOfCurrentCC) {
-	bool resultForCurrent = false; // if true then this vertice has an in-edge
 	colors[current] = 1;
 	for (int i = 0; i < graph[current].size(); ++i) {
 		int currentV = graph[current][i];
 		if (colors[currentV] == 0)
-			resultForCurrent = resultForCurrent || dfs2(graph, currentV, colors, components, numberOfCurrentCC);
-		else {
-			if (colors[currentV] == 2)
-				resultForCurrent = true;
-		}
+			dfs2(graph, currentV, colors, components, numberOfCurrentCC);
 	}
 	colors[current] = 2;
 	if (numberOfCurrentCC >= components.size()) {
@@ -75,7 +85,6 @@ bool dfs2(std::vector<std::vector<int>> &graph,
 		components.push_back(toAdd);
 	} else
 		components[numberOfCurrentCC].push_back(current);
-	return resultForCurrent;
 }
 
 void makeInvertedGraph(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &invertedGraph) {
