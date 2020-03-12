3f093bfa719b6e8bcd13848047bfa07d41dcc95c
MarkTiukov <tyukov.mark@gmail.com>
2020-03-13 01:54:13 +0300

    Task3 is done

--- Contest1/Task3.cpp
+++ Contest1/Task3.cpp
@@ -5,9 +5,13 @@ void findSCC(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>>
 void dfs1(std::vector<std::vector<int>> &graph, int current, std::vector<int> &colors, std::vector<int> &vertices);
 void makeInvertedGraph(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &invertedGraph);
 void dfs2(std::vector<std::vector<int>> &graph, int current, std::vector<int> &colors, std::vector<std::vector<int>> &components, const int &numberOfCurrentCC, bool& result);
-int countToAdd(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &components);
+int countToAdd(std::vector<std::vector<int>> &graph);
+void countOutGoingEdges(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &components, std::vector<int> &colors);
 
-int ccIn = 0;
+
+int ccIn = 0; // number of cc with in-edges
+int ccOut = 0; // number of c with out-edges
+int maxColor = 2;
 
 int main() {
 	int n, m;
@@ -20,11 +24,31 @@ int main() {
 	}
 	std::vector<std::vector<int>> components;
 	findSCC(graph, components);
-	std::cout << "ccIn == " << ccIn << std::endl;
+	std::cout << countToAdd(components) << std::endl;
 }
 
-int countToAdd(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &components) {
+int countToAdd(std::vector<std::vector<int>> &graph) {
+	if (graph.size() > 1) {
+		return std::max(ccIn, ccOut);
+	}
+	return 0;
+}
 
+void countOutGoingEdges(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &components, std::vector<int> &colors) {
+	for (int i = 0; i < components.size(); ++i) {
+		bool test = true;
+		for (int j = 0; j < components[i].size(); ++j) {
+			if (test) {
+				for (int k = 0; k < graph[components[i][j]].size(); ++k) {
+					if (test && colors[graph[components[i][j]][k]] != colors[components[i][0]]) {
+						test = false;
+					}
+				}
+			}
+		}
+		if (test)
+			ccOut++;
+	}
 }
 
 void findSCC(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &components) {
@@ -39,42 +63,19 @@ void findSCC(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>>
 	}
 	std::vector<std::vector<int>> invertedGraph(graph.size());
 	makeInvertedGraph(graph, invertedGraph);
-	/*
-	std::cout << "INVERTED GRAPH:" << std::endl;
-	for (int i = 0; i < invertedGraph.size(); ++i) {
-		for (int j : invertedGraph[i]) {
-			std::cout << i << " " << j << std::endl;
-		}
-	}
-	std::cout << "/////////////////////////" << std::endl;
-	 */
 	colors = std::vector<int>(graph.size(), 0);
 	int numberOfCurrentCC = 0;
-	// checking vertices
-	/*
-	for (int i : vertices)
-	std::cout << i << std::endl;
-	*/
 	for (int i = vertices.size() - 1; i > -1; --i) {
-
 		if (colors[vertices[i]] == 0) {
-			bool result = false;
+			bool result = true;
 			dfs2(invertedGraph, vertices[i], colors, components, numberOfCurrentCC, result);
 			if (result)
 				ccIn++;
 			numberOfCurrentCC++;
+			maxColor++;
 		}
 	}
-	// printing components
-	/*
-	for (int i = 0; i < components.size(); ++i) {
-		std::cout << "Component number " << i << ": ";
-		for (int j = 0; j < components[i].size(); ++j) {
-			std::cout << components[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-	*/
+	countOutGoingEdges(graph, components, colors);
 }
 
 void dfs2(std::vector<std::vector<int>> &graph,
@@ -85,14 +86,14 @@ void dfs2(std::vector<std::vector<int>> &graph,
 	colors[current] = 1;
 	for (int i = 0; i < graph[current].size(); ++i) {
 		int currentV = graph[current][i];
-		if (colors[currentV] == 2) {
-			result = true;
+		if (colors[currentV] > 1 && colors[currentV] != maxColor) {
+			result = false;
 		}
 		if (colors[currentV] == 0) {
 			dfs2(graph, currentV, colors, components, numberOfCurrentCC, result);
 		}
 	}
-	colors[current] = 2;
+	colors[current] = maxColor;
 	if (numberOfCurrentCC >= components.size()) {
 		std::vector<int> toAdd;
 		toAdd.push_back(current);
