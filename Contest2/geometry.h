2291fd5a3b7a282d33b9fb161941c165e85816e7
MarkTiukov <tyukov.mark@gmail.com>
2020-04-09 02:22:32 +0300

    Finished Polygon::area() and Polygon::verticesCount() Plus copied code from deleted branch Finished there: empty classes; struct Point class Line; Polygon::perimeter();

--- Contest2/geometry.h
+++ Contest2/geometry.h
@@ -45,9 +45,9 @@ class Shape {
 	virtual double perimeter() const = 0;
 	virtual double area() const = 0;
 //  virtual bool operator==(const Shape &another) = 0;
-//  virtual bool isCongruentTo(const Shape &another) = 0;
-//    virtual bool isSimilarTo(const Shape &another) = 0;
-//    virtual bool containsPoint(Point point) = 0;
+//  virtual bool isCongruentTo(const Shape &another) const = 0;
+//    virtual bool isSimilarTo(const Shape &another) const = 0;
+//    virtual bool containsPoint(Point point) const = 0;
 //    virtual void rotate(Point center, double angle) = 0;
 //    virtual void reflex(Point center) = 0;
 //    virtual void reflex(Line axis) = 0;
