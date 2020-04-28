d3bd40966b680536bd7885fa2c7cb2e3354a992a
MarkTiukov <tyukov.mark@gmail.com>
2020-04-28 18:58:01 +0300

    Created a folder for the 3d contest

---
+++ Contest3/cmake-build-debug/CMakeFiles/Contest3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Contest3.dir/main.cpp.o"
+  "Contest3"
+  "Contest3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Contest3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
