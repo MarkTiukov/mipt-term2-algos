de162200bd92eccba7fab5707b0dd1be11b27d09
MarkTiukov <tyukov.mark@gmail.com>
2020-04-28 20:12:48 +0300

    Fixed remarks

--- Contest2/Task3.cpp
+++ Contest2/Task3.cpp
@@ -1,94 +1,92 @@
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
 #include <iomanip>
+#include <iostream>
+#include <queue>
+#include <vector>
 
-int start, finish;
 const double MAX_VALUE = -2e9;
 
 struct Graph {
-	std::vector<std::vector<std::pair<int, double>>> graph;
+  std::vector<std::vector<std::pair<int, double>>> graph;
 
-	Graph() { this->graph = std::vector<std::vector<std::pair<int, double>>>(); }
-	Graph(int n) : graph(n) {}
+  Graph() { this->graph = std::vector<std::vector<std::pair<int, double>>>(); }
+  Graph(int n) : graph(n) {}
 
-	void add(int from, int to, double weight);
-	void print();
-	int size();
+  void add(int from, int to, double weight);
+  void print();
+  int size();
 
 };
 
 struct Node {
-	int number;
-	double distance = MAX_VALUE;
+  int number;
+  double distance = MAX_VALUE;
 
-	Node(int number, double distance) : number(number), distance(distance) {}
+  Node(int number, double distance) : number(number), distance(distance) {}
 };
 
-class myComparator {
+class MyComparator {
  public:
-	int operator()(const Node &a, const Node &b) {
-		return a.distance < b.distance;
-	}
+  int operator()(const Node &a, const Node &b) {
+	return a.distance < b.distance;
+  }
 };
 
-void dijkstra(Graph &graph, std::vector<double> &dist);
+void dijkstra(Graph &graph, std::vector<double> &dist, int start);
 
 int main() {
-	int n, m;
-	std::cin >> n >> m >> start >> finish;
-	start--;
-	finish--;
-	Graph graph(n);
-	for (int i = 0; i < m; ++i) {
-		int from, to;
-		double cost;
-		std::cin >> from >> to >> cost;
-		cost = 1.0 - cost / 100.0;
-		--from;
-		--to;
-		graph.add(from, to, cost);
-		graph.add(to, from, cost);
-	}
-	std::vector<double> dist(n, MAX_VALUE);
-	dist[start] = 1;
-	dijkstra(graph, dist);
-	std::cout << std::setprecision(6) << 1 - dist[finish] << std::endl;
+  int start, finish;
+  int n, m;
+  std::cin >> n >> m >> start >> finish;
+  start--;
+  finish--;
+  Graph graph(n);
+  for (int i = 0; i < m; ++i) {
+	int from, to;
+	double cost;
+	std::cin >> from >> to >> cost;
+	cost = 1.0 - cost / 100.0;
+	--from;
+	--to;
+	graph.add(from, to, cost);
+	graph.add(to, from, cost);
+  }
+  std::vector<double> dist(n, MAX_VALUE);
+  dist[start] = 1;
+  dijkstra(graph, dist, start);
+  std::cout << std::setprecision(6) << 1 - dist[finish] << std::endl;
 }
 
 void Graph::add(int from, int to, double weight) {
-	if (to != -1)
-		this->graph[from].push_back(std::make_pair(to, weight));
+  if (to != -1)
+	this->graph[from].push_back(std::make_pair(to, weight));
 }
 
 void Graph::print() {
-	for (int from = 0; from < this->graph.size(); ++from) {
-		for (auto pair: this->graph[from]) {
-			std::cout << "from " << from << " to " << pair.first << " with weight " << pair.second << std::endl;
-		}
+  for (int from = 0; from < this->graph.size(); ++from) {
+	for (auto pair: this->graph[from]) {
+	  std::cout << "from " << from << " to " << pair.first << " with weight " << pair.second << std::endl;
 	}
+  }
 }
 
 int Graph::size() {
-	return this->graph.size();
+  return this->graph.size();
 }
 
-void dijkstra(Graph &graph, std::vector<double> &dist) {
-	std::priority_queue<Node, std::vector<Node>, myComparator> queue;
-	queue.emplace(Node(start, 1));
-	while (queue.size() > 0) {
-		Node current = queue.top();
-		queue.pop();
-		if (current.distance == dist[current.number]) {
-			for (auto neighbor: graph.graph[current.number]) {
-				double alternative = dist[current.number] * neighbor.second;
-				if (alternative > dist[neighbor.first]) {
-					dist[neighbor.first] = alternative;
-					queue.push(Node(neighbor.first, alternative));
-				}
-			}
+void dijkstra(Graph &graph, std::vector<double> &dist, int start) {
+  std::priority_queue<Node, std::vector<Node>, MyComparator> queue;
+  queue.emplace(Node(start, 1));
+  while (queue.size() > 0) {
+	Node current = queue.top();
+	queue.pop();
+	if (current.distance == dist[current.number]) {
+	  for (auto neighbor: graph.graph[current.number]) {
+		double alternative = dist[current.number] * neighbor.second;
+		if (alternative > dist[neighbor.first]) {
+		  dist[neighbor.first] = alternative;
+		  queue.push(Node(neighbor.first, alternative));
 		}
+	  }
 	}
+  }
 }
