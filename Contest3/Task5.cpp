379dd337dedc7d1d3214d1f6c4f11f13868debb7
MarkTiukov <tyukov.mark@gmail.com>
2020-05-10 03:17:02 +0300

    Added reading

--- Contest3/Task5.cpp
+++ Contest3/Task5.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
-int main() {
+struct Edge {
+  int start, finish;
+  int flow, capacity;
 
+};
+
+int main() {
+  int n, m, start, finish;
+  std::cin >> n >> m >> start >> finish;
+  for (int i = 0; i < m; ++i) {
+    int from, to;
+    std::cin >> from >> to;
+    
+  }
 }
