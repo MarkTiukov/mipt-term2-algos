e9a3b1abacec647562b444bc41472998ac3fdd42
MarkTiukov <tyukov.mark@gmail.com>
2020-05-17 13:37:47 +0300

    Initial commit

---
+++ Contest4/Task1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  int n, m;
+  std::cin >> n >> m;
+  for (int i = 0; i < n; ++i) {
+    int current_number;
+    std::cin >> current_number;
+
+  }
+  for (int i = 0; i < m; ++i) {
+    int start, end;
+    std::cin >> start >> end;
+  }
+}
