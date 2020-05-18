4774cfe152afaa51e282e20ee434fd7b41b88c9d
MarkTiukov <tyukov.mark@gmail.com>
2020-05-19 02:04:06 +0300

    Started constructors

--- Contest3/list.h
+++ Contest3/list.h
@@ -1,18 +1,28 @@
-template<typename T>
-struct Node {
-  T value;
-  Node *next = nullptr;
-  Node *previous = nullptr;
-
-  Node(T value) : value(value) {}
-  Node(T value, Node *previous) : value(value), previous(previous) {}
-
-};
+#include <utility>
 
 template<typename T>
 class List {
- private:
-
  public:
+  struct Node {
+	T value;
+	Node* next = nullptr;
+	Node* previous = nullptr;
 
+	Node() = default;
+	Node(T value) : value(value) {}
+	Node(T value, Node* previous) : value(value), previous(previous) {}
+	Node(T value, Node* previous, Node* next) : value(value), previous(previous), next(next) {}
+  };
+ private:
+  size_t size_ = 0;
+  Node* first_ = nullptr;
+  Node* last_ = nullptr;
+ public:
+  List() = default;
+  List(size_t count, const T& value = T()) : size_(count) {
+    if (this->size_ > 0) {
+    	this->first_ = new Node(value);
+    }
+
+  }
 };
--- Contest3/main.cpp
+++ Contest3/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "list.h"
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
